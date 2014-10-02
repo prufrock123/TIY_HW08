3ec8b910a9460ba020566fe2e6662e9ea5036962
prufrock123 <shawnmllr86@gmail.com>
2014-10-02 08:35:18 -0500

    does multiply.

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ function multiply(array){
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
+function reverse(string){
     "use strict";
     //...
 }
